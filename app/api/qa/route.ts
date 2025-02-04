e4ad5108f389f3348f6b0755553c9895e8f6907d
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-02-03 21:56:48 -0800

    added google genai

--- app/api/qa/route.ts
+++ app/api/qa/route.ts
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
-import { QAGenerationChain } from "langchain/chains";
-import { GoogleGenerativeAI } from "langchain/llms/google_generative_ai";
+// import { QAGenerationChain } from "langchain/chains";
+// import { GoogleGenerativeAI } from "langchain/llms/google_generative_ai";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 export async function POST(req: Request) {
   const { text } = await req.json();
 
-  const llm = new GoogleGenerativeAI();
+  const llm = new ChatGoogleGenerativeAI({
+    model: "gemini-1.5-pro",
+    temperature: 0,
+    maxRetries: 2,
+    // other params...
+  });
   const qaChain = new QAGenerationChain({ llm });
 
   const qaPairs = await qaChain.run(text);
